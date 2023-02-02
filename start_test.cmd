34dea266b0f4e7b04e2f52148408614db4eeaf31
Yaroslav <98458281+mangoodd@users.noreply.github.com>
2023-02-02 11:03:51 +0300

    Add files via upload

---
+++ start_test.cmd
@@ -0,0 +1,11 @@
+csvreader.exe CSVFiles\file_def.csv
+csvreader.exe CSVFiles\file.csv
+csvreader.exe CSVFiles\fl.csv
+csvreader.exe file.css
+csvreader.exe
+csvreader.exe file.csv
+csvreader.exe CSVFiles\another.csv
+csvreader.exe CSVFiles\tabl_err1.csv
+csvreader.exe CSVFiles\tabl_err2.csv
+
+pause
